0932fa4e2932f11301fd298e95cd2966f0b1ed52
adhinnugroho <adhinnnugroho@gmail.com>
2023-11-26 21:26:51 +0700

    update views home

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,5 +1,34 @@
 <x-guest-layout>
     @livewire('layouts.navigations-bar')
 
+    <div class="lg:ml-10 ml-5 lg:mr-10 mt-10">
+        <div class="grid grid-cols-1 gap-6" >
+            <div class="bg-gray-200 shadow-lg rounded-lg h-56" style="background-image: url(https://images.unsplash.com/photo-1577495508048-b635879837f1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80);background-attachment: fixed;">
+
+            </div>
+        </div>
+    </div>
+
+
+    <div class="mt-10 ml-10 mr-10">
+        <div class="grid grid-cols-1 lg:grid-cols-3 gap-6">
+            <div class="bg-gray-200 shadow-lg h-44">
+                <h5 class="pt-5 pl-5">
+                    Baca Qur'an
+                </h5>
+            </div>
+            <div class="bg-gray-200 shadow-lg h-44">
+                <h5 class="pt-5 pl-5">
+                    Baca Qur'an
+                </h5>
+            </div>
+            <div class="bg-gray-200 shadow-lg h-44">
+                <h5 class="pt-5 pl-5">
+                    Baca Qur'an
+                </h5>
+            </div>
+        </div>
+    </div>
+
     {{-- @livewire('juz.alquranjus') --}}
 </x-guest-layout>
